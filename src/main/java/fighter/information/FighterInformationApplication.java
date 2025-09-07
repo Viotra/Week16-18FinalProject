1ce692d0a2b024e50ef9fe9ceda149855ecf82d9
Timothy Abel <abeltimothy1@gmail.com>
2025-09-06 22:51:07 -0600

    Added information about Yaml file and POM file to main application.

--- src/main/java/fighter/information/FighterInformationApplication.java
+++ src/main/java/fighter/information/FighterInformationApplication.java
@@ -3,6 +3,10 @@ package fighter.information;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* My pom file has Spring JPA, Spring Web Tools, MySQL Connector J, and Lombok only*/
+//Data-source is set to update and SQL init is set to never in YAML file.
+
+
 @SpringBootApplication
 public class FighterInformationApplication {
 	public static void main(String[] args) {
